d6562a775e84d39a4ae3bec3579bd07a5a23d8d9
Sathish <Sathish@DESKTOP-NJ5HU2S>
2024-08-02 21:07:04 -0700

    project commit

--- src/test/java/GoogleLoginAccount/createAccount.java
+++ src/test/java/GoogleLoginAccount/createAccount.java
@@ -1,7 +1,13 @@
 package GoogleLoginAccount;
 
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Scanner;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,41 +15,73 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class createAccount {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {	
 		String Url="https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.google.com"
 				+ "%3Fhl%3Den-US&ec=GAlA8wE&hl=en&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-"
 				+ "1241331482%3A1722311049648081&ddm=0";
 		String nextButton="//span[text()='Next']";
-		String userName ="//input[@name='Username']";
-		String passWord="/input[@name='Passwd']";
-		String conifirmPassword="//input[@name='PasswdAgain']";
-		
+
 		WebDriver  driver= new ChromeDriver();
 		driver.get(Url);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
-		driver.findElement(By.xpath("//span[text()='Create account']")).click();
-		driver.findElement(By.xpath("//span[text()='For my personal use']")).click();
-		driver.findElement(By.name("firstName")).sendKeys("sharp");
-		driver.findElement(By.xpath(nextButton)).click();
-		WebElement month=driver.findElement(By.xpath("//select[@aria-labelledby='month-label']"));
-		Select selectMonth= new Select(month);
-		selectMonth.selectByVisibleText("March");
-		driver.findElement(By.xpath("//input[@name='day']")).sendKeys("5");
-		driver.findElement(By.xpath("//input[@name='year']")).sendKeys("1998");
-		WebElement Gender= driver.findElement(By.xpath("//select[@aria-labelledby='gender-label']"));
-		Select selectGender= new Select(Gender);
-		selectGender.selectByVisibleText("Male");
-		driver.findElement(By.xpath(nextButton)).click();
-		driver.findElement(By.xpath(userName)).sendKeys("sharpmind9703");
-		driver.findElement(By.xpath(nextButton)).click();
-		driver.findElement(By.xpath(passWord)).sendKeys("SharpMind@2268");
-		driver.findElement(By.xpath(conifirmPassword)).sendKeys("SharpMind@2268");
-		driver.findElement(By.xpath(nextButton)).click();
-		driver.findElement(By.xpath("//span[text()='Phone number']")).sendKeys("9092654591");
-		driver.findElement(By.xpath(nextButton)).click();
-		driver.findElement(By.xpath(nextButton)).click();
-		driver.findElement(By.xpath("//span[text()='I agree']")).click();
+		String filelocation="C:\\Users\\Sathish\\Documents\\createaccount data.xlsx";
+		XSSFWorkbook wbook = new XSSFWorkbook(filelocation);
+		XSSFSheet sheet = wbook.getSheet("Sheet1");
+		int countrow = sheet.getLastRowNum();
+		int countcolumn= sheet.getRow(1).getLastCellNum();
+
+		for (int i = 1; i <= countrow; i++) {
+			XSSFRow celldata = sheet.getRow(i);
+			String firstname = null;
+			String userName = null;
+			String passWd=null;
+			String phonenumber = null;
+			String confirmPasswd=null;
+			String month=null;
+			String day=null;
+			String year=null;
+			String gender=null;
+
+			for (int j = 0; j < countcolumn; j++) {
+				XSSFCell cell = celldata.getCell(j);
+				firstname=  celldata.getCell(0).getStringCellValue();
+				month=  celldata.getCell(1).getStringCellValue();
+				day= String.valueOf((long)celldata.getCell(2).getNumericCellValue());
+				year= String.valueOf((long)celldata.getCell(3).getNumericCellValue());
+				gender= celldata.getCell(4).getStringCellValue();
+				userName= celldata.getCell(5).getStringCellValue();
+				passWd= celldata.getCell(6).getStringCellValue();
+				confirmPasswd= celldata.getCell(7).getStringCellValue();
+				phonenumber=  String.valueOf((long)celldata.getCell(8).getNumericCellValue()); 
+			}
+			driver.findElement(By.xpath("//span[text()='Create account']")).click();
+			driver.findElement(By.xpath("//span[text()='For my personal use']")).click();
+			driver.findElement(By.name("firstName")).sendKeys(firstname);
+			driver.findElement(By.xpath(nextButton)).click();
+			WebElement Month1=driver.findElement(By.xpath("//select[@aria-labelledby='month-label']"));
+			Select selectMonth= new Select(Month1);
+			selectMonth.selectByVisibleText(month);
+			driver.findElement(By.xpath("//input[@name='day']")).sendKeys(day);
+			driver.findElement(By.xpath("//input[@name='year']")).sendKeys(year);
+			WebElement Gender= driver.findElement(By.xpath("//select[@aria-labelledby='gender-label']"));
+			Select selectGender= new Select(Gender);
+			selectGender.selectByVisibleText("Male");
+			driver.findElement(By.xpath(nextButton)).click();
+			driver.findElement(By.xpath("//input[@name='Username']")).sendKeys(userName);
+			driver.findElement(By.xpath(nextButton)).click();
+			driver.findElement(By.xpath("/input[@name='Passwd']")).sendKeys(passWd);
+			driver.findElement(By.xpath("//input[@name='PasswdAgain']")).sendKeys(confirmPasswd);
+			driver.findElement(By.xpath(nextButton)).click();
+			driver.findElement(By.xpath("//span[text()='Phone number']")).sendKeys(phonenumber);
+			driver.findElement(By.xpath(nextButton)).click();
+			driver.findElement(By.xpath(nextButton)).click();
+			driver.findElement(By.xpath("//span[text()='I agree']")).click();
+			System.out.println(firstname+"-->"+passWd+"-->"+phonenumber);
+			System.out.println();
+		}
+		System.out.println("rowcount-->"+countrow +"colcount-->"+countcolumn);
+
 
 
 
--- src/test/java/GoogleLoginAccount/logIn.java
+++ src/test/java/GoogleLoginAccount/logIn.java
@@ -1,7 +1,12 @@
 package GoogleLoginAccount;
 
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Scanner;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,7 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class logIn {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		WebDriver  driver= new ChromeDriver();
 		driver.get("https://accounts.google.com/v3/signin"
 				+ "/identifier?continue=https%3A%2F%2Fwww.google.com%3Fhl%3Den-US&ec"
@@ -17,10 +22,27 @@ public class logIn {
 				+ "S-1241331482%3A1722311049648081&ddm=0");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
-		driver.findElement(By.xpath("//input[@aria-label='Email or phone']")).sendKeys("sharpmind9703@gmail.com");
-		driver.findElement(By.xpath("//span[text()='Next']")).click();
-		driver.findElement(By.xpath("//input[@name='Passwd']")).sendKeys("SharpMind@2268");
-		driver.findElement(By.xpath("//span[text()='Next']")).click();
+		String filelocation="C:\\Users\\Sathish\\Documents\\Test Data.xlsx";
+		XSSFWorkbook wbook = new XSSFWorkbook(filelocation);
+		XSSFSheet sheet = wbook.getSheet("Sheet1");
+		int countrow = sheet.getLastRowNum();
+		int countcolumn= sheet.getRow(1).getLastCellNum();
+		System.out.println("rowcount-->"+countrow +"colcount-->"+countcolumn);
+		for (int i = 1; i <= countrow; i++) {
+			XSSFRow celldata = sheet.getRow(i);
+			String eMail=  celldata.getCell(0).getStringCellValue();
+			String passWord=  celldata.getCell(1).getStringCellValue();
+			String phonenumber=  String.valueOf((long)celldata.getCell(2).getNumericCellValue()); 
+			driver.findElement(By.xpath("//input[@aria-label='Email or phone']")).sendKeys(eMail);
+			driver.findElement(By.xpath("//span[text()='Next']")).click();
+			driver.findElement(By.xpath("//input[@name='Passwd']")).sendKeys(passWord);
+			driver.findElement(By.xpath("//span[text()='Next']")).click();
+			driver.findElement(By.xpath("//input[@class='VfPpkd-fmcmS-wGMbrd ']")).sendKeys(phonenumber);
+			driver.findElement(By.xpath("//span[text()='Next']")).click();
+			System.out.println(eMail + "--"+passWord +"--"+phonenumber);
+		}
+		System.out.println("Done");
+
 
 	}
 }
